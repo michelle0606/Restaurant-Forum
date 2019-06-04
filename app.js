5a1b27b23717a2719cf6f37580f030f2e34ecca8
walle <michelle210181@gmail.com>
2019-06-04 14:30:20 +0800

    Add user registration feature

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const db = require('./models')
 
+const flash = require('connect-flash')
+const session = require('express-session')
+
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'pug')
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.listen(3000, () => {
   db.sequelize.sync()
