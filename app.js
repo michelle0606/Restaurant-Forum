7cc41e1eb71d7296fc5292112ff3ae86ad8e897f
walle <michelle210181@gmail.com>
2019-06-04 12:35:41 +0800

    add mysql database and User table

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const db = require('./models')
 
 app.set('view engine', 'pug')
 
 app.listen(3000, () => {
+  db.sequelize.sync()
   console.log(`Example app listening on port 3000!`)
 })
+
+require('./routes')(app)
