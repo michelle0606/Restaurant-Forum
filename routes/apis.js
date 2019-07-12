884f0bcd15d20e468b9722a025bbf998f61d1413
walle <michelle210181@gmail.com>
2019-07-12 15:34:45 +0800

    add authenticated and authenticatedAdmin

--- routes/apis.js
+++ routes/apis.js
@@ -7,26 +7,80 @@ const adminController = require('../controllers/api/adminController.js')
 const categoryController = require('../controllers/api/categoryController.js')
 const userController = require('../controllers/api/userController.js')
 
+const passport = require('../config/passport')
+
+const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) {
+      return next()
+    }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+
 // /admin/restaurants
-router.get('/admin/restaurants', adminController.getRestaurants)
-router.get('/admin/restaurants/:id', adminController.getRestaurant)
+router.get(
+  '/admin/restaurants',
+  authenticated,
+  authenticatedAdmin,
+  adminController.getRestaurants
+)
+router.get(
+  '/admin/restaurants/:id',
+  authenticated,
+  authenticatedAdmin,
+  adminController.getRestaurant
+)
 router.post(
   '/admin/restaurants',
   upload.single('image'),
+  authenticated,
+  authenticatedAdmin,
   adminController.postRestaurant
 )
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+router.delete(
+  '/admin/restaurants/:id',
+  authenticated,
+  authenticatedAdmin,
+  adminController.deleteRestaurant
+)
 router.put(
   '/admin/restaurants/:id',
   upload.single('image'),
+  authenticated,
+  authenticatedAdmin,
   adminController.putRestaurant
 )
 
 // /admin/categories
-router.get('/admin/categories', categoryController.getCategories)
-router.post('/admin/categories', categoryController.postCategory)
-router.put('/admin/categories/:id', categoryController.putCategory)
-router.delete('/admin/categories/:id', categoryController.deleteCategory)
+router.get(
+  '/admin/categories',
+  authenticated,
+  authenticatedAdmin,
+  categoryController.getCategories
+)
+router.post(
+  '/admin/categories',
+  authenticated,
+  authenticatedAdmin,
+  categoryController.postCategory
+)
+router.put(
+  '/admin/categories/:id',
+  authenticated,
+  authenticatedAdmin,
+  categoryController.putCategory
+)
+router.delete(
+  '/admin/categories/:id',
+  authenticated,
+  authenticatedAdmin,
+  categoryController.deleteCategory
+)
 
 // user action
 router.post('/signin', userController.signIn)
